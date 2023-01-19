48b9ce7990466a53f6c707a6ac3be8ace10cc803
uttamkrshaw <uttamkr5599@gmail.com>
2023-01-19 23:38:33 +0530

    home page woriking

---
+++ bloom-beauty/src/Components/Pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import DefaultNavbar from "../Main/Header";
+import Part1 from "./HomePage/Part1";
+import Part2 from "./HomePage/Part2";
+
+function HomePage(){
+    return(
+        <>
+        <DefaultNavbar />
+        <Part1 />
+        <Part2 />
+        </>
+    )
+}
+export default HomePage;
