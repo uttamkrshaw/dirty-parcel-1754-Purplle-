89ea07fd0c37941a92ba380472f38d6b322b2fbb
uttamkrshaw <uttamkr5599@gmail.com>
2023-05-06 23:59:32 +0530

    Fresh Start

---
+++ Frontend/src/App.js
@@ -0,0 +1,10 @@
+import AllRouters from "./Components/Router/AllRoutes";
+function App() {
+  return (
+    <div className="App">
+      <AllRouters />
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/src/Components/Pages/Product/ProductPage.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom"
+import { Card, CardHeader, CardBody, CardFooter, Image, Stack, Heading, Text, Divider, ButtonGroup, Button , Box } from '@chakra-ui/react'
+import axios from 'axios';
+import DefaultNavbar from "../../Main/Header";
+import DefaultFooter from "../../Main/Footer";
+
+// testing 
+import { ProductCard } from "./ProductComponents/ProductCard";
+import { ProductGrid } from "./ProductComponents/ProductGrid";
+
+function getData() {
+    return axios.get(`http://localhost:3000/products?_limit=50`)
+}
+
+function Product() {
+    // on component mount make a upi call get the data & display the data.
+    // as soon as someone clicks on the card u move to a new page where u will get more data.
+    const [loading, setLoading] = React.useState(false);
+    const [data, setData] = React.useState([]);
+    const [error, setError] = React.useState(false);
+    // const isAuth = true;
+    //  const nav = useNavigate();
+
+
+    const fetchHandleData = () => {
+        setLoading(true);
+        getData()
+            .then((res) => { setLoading(false); setData(res.data) })
+            .catch((err) => { setLoading(false); setError(true) })
+    }
+
+    React.useEffect(() => {
+        fetchHandleData();
+    }, [])
+
+    // if (!isAuth) {
+    //     return <Navigate to="/" />
+    // }
+
+
+    console.log(data);
+
+
+    return (
+        loading ? <h1>Ruko Jara Sabar Karo</h1> : error ? <h1>Ruka Bhai Phr ja aur check kara sb</h1> : <div>
+            <div>
+                {/* <button onClick={()=>nav("/")}>Go to home page</button> */}
+                <DefaultNavbar />
+            </div>
+            <div>
+                <Box bg={'#EEEEEE'}>
+                <Box
+                    maxW="7xl"
+                    mx="auto"
+                    px={{
+                        base: '4',
+                        md: '8',
+                        lg: '12',
+                    }}
+                    py={{
+                        base: '6',
+                        md: '8',
+                        lg: '12',
+                    }}
+                >
+                    <ProductGrid>
+                        {data.map((product) => (
+                            <ProductCard key={product.id} product={product} />
+                        ))}
+                    </ProductGrid>
+                </Box>
+                </Box>
+            </div>
+
+        </div>
+    )
+}
+export default Product;
