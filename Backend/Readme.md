c44d1470a9b82119a56cae8d2b138c4760478a94
uttamkrshaw <uttamkr5599@gmail.com>
2023-05-11 01:04:05 +0530

    User Model Created & working

---
+++ Backend/Readme.md
@@ -0,0 +1,20 @@
+{
+"username":"UttamKr",
+"email":"uttamkr5599@gmail.com",
+"password":"12345678",
+"age":25,
+"location":{
+ "Name":"Uttam Kr Shaw",
+ "Mobile_No" :9091390251,
+ "Pin_Code" :828202,
+ "Address" :"House no :225, Ward No 3, Near Middle School",
+ "District":"Dhanbad",
+ "State":"Jharkhand",
+ "Address_Type":"Home"
+},
+"type":"ADMIN",
+"order": {
+    "ORDERS":[],
+    "FAV":[]
+}
+}
--- Backend/Routes/user.routes.js
+++ Backend/Routes/user.routes.js
@@ -1,37 +1,46 @@
 const express = require("express")
 const userRouter = express.Router()
+const {UserModel} = require("../Model/user.model")
 
 
 // --------------->>>>>>>> Get User List <<<<<<<<-------------------
 
-userRouter.get("/list",(req,res)=>{
+userRouter.get("/list", (req, res) => {
     res.send("GET USER LIST")
 })
 
 // --------------->>>>>>>> User Registration <<<<<<<<-------------------
 
-userRouter.post("/register",(req,res)=>{
-    res.send("NEW USER REGISTRATION")
+userRouter.post("/register", async (req, res) => {
+    const user = new UserModel(req.body)
+    try {
+        await user.save()
+        res.status(200).send("NEW USER REGISTRATION")
+    } catch (error) {
+        res.status(400).send({"msg": error.message})
+    }
 })
 
 // --------------->>>>>>>> User Login <<<<<<<<-------------------
 
 
-userRouter.post("/login",(req,res)=>{
+userRouter.post("/login", (req, res) => {
     res.send("USER LOGIN SUCCESSFUL!")
 })
 
 // --------------->>>>>>>> User Details Update <<<<<<<<-------------------
 
-userRouter.patch("/update",(req,res)=>{
+userRouter.patch("/update", (req, res) => {
     res.send("USER DETAILS UPDATED!")
 })
 
 // --------------->>>>>>>> User Delete <<<<<<<<-------------------
 
-userRouter.delete("/register",(req,res)=>{
+userRouter.delete("/register", (req, res) => {
     res.send("USER DETAILS REMOVED")
 })
 
 
-module.exports = {userRouter}
+module.exports = {
+    userRouter
+}
