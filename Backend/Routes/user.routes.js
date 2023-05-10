ccf0128cb61b3b591a1c5041b215f06ddce47cbc
uttamkrshaw <uttamkr5599@gmail.com>
2023-05-11 02:43:48 +0530

    Working on nodemailer

--- Backend/Routes/user.routes.js
+++ Backend/Routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const userRouter = express.Router()
 const {UserModel} = require("../Model/user.model")
+const { sendEmail } = require("../nodemailer")
 
 
 // --------------->>>>>>>> Get User List <<<<<<<<-------------------
@@ -12,12 +13,18 @@ userRouter.get("/list", (req, res) => {
 // --------------->>>>>>>> User Registration <<<<<<<<-------------------
 
 userRouter.post("/register", async (req, res) => {
-    const user = new UserModel(req.body)
-    try {
-        await user.save()
-        res.status(200).send("NEW USER REGISTRATION")
-    } catch (error) {
-        res.status(400).send({"msg": error.message})
+    const {username, email} = req.body
+    const userfound = await UserModel.find({username, email})
+    if (userfound.length === 0) {
+        const user = new UserModel(req.body)
+        try {
+            await user.save()
+            res.status(200).send("NEW USER REGISTRATION")
+        } catch (error) {
+            res.status(400).send({"msg": error.message})
+        }
+    } else {
+        res.status(400).send({"msg": "User Already Exists with Same Credentials. Pls login with same Credentials or Enter new Credentials to Create a Account"})
     }
 })
 
---
+++ Backend/nodemailer.js
@@ -0,0 +1,44 @@
+const nodemailer = require("nodemailer");
+async function sendEmail(data) {
+  //  console.log( "data " ,data);
+  //   transpoter to send/transport email
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+      user: 'uttamkr5599@gmail.com',
+      pass: "hokjxwjymqzkqdbj"
+    }
+  });
+
+  transporter.sendMail({
+    to: `${data.email}`,
+    from: 'chikkuuu@gmail.com',
+    subject: data.subject,
+    html: data.body,
+  })
+    .then(() => console.log('mail sent successfully'))
+    .catch((err) => console.log("err", err))
+
+}
+
+module.exports = { sendEmail, }
+
+
+// ! -----must have data in while calling sendEmail() -----
+
+// data = {
+//     email : "fsociety430@gmail.com",
+//     subject : "somehting happend",
+//     body : "email data cloud be in html/text / json /image"
+// }
+
+
+// sending mails
+
+ const otp = Math.round((Math.random() * 9999))
+ sendEmail({ email:'uttamkr5599@gmail.com', subject: "Login OTP", body: ` OTP is ${otp}` })
+
+
+// GoogleKey = 
