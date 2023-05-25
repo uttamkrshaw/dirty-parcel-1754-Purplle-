31c09f402e736a0c51d6732d89e308212c11d7d6
uttamkrshaw <uttamkr5599@gmail.com>
2023-05-25 16:42:40 +0530

    added get routs in product router in backend

--- Backend/Routes/product.routes.js
+++ Backend/Routes/product.routes.js
@@ -5,28 +5,17 @@ const {auth} = require("../Middleware/auth.middleware")
 const {admin} = require("../Middleware/admin.middleware")
 
 
-// --------------->>>>>>>> Get Product List <<<<<<<<-------------------
+// --------------->>>>>>>> Get All Product List <<<<<<<<-------------------
 
 productRouter.get("/getall", auth, async (req, res) => {
     try {
-        // const {page,category} = req.query
-        // let skip
-        // if (page)
-        //     skip = (page - 1) * 30;
-        // else
-        //     skip = 0
-
-        // const product = await ProductModel.find({
-        //     category: {
-        //         $regex: `(?i)${category}`
-        //     }
-        // }).skip(skip).limit(30);
         const {page} = req.query
         let skip
         if (page) 
             skip = (page - 1) * 30;
          else 
             skip = 0
+
         const product = await ProductModel.find().skip(skip).limit(30);
         res.status(200).send(product)
     } catch (error) {
@@ -34,6 +23,110 @@ productRouter.get("/getall", auth, async (req, res) => {
     }
 })
 
+
+// --------------->>>>>>>> Get Specific Category Product List <<<<<<<<-------------------
+
+productRouter.get("/getcat", auth, async (req, res) => {
+    try {
+        const {page, category} = req.query
+        let skip
+        if (page) 
+            skip = (page - 1) * 30;
+         else 
+            skip = 0
+
+        const product = await ProductModel.find({
+            category: {
+                $regex: `(?i)${category}`
+            }
+        }).skip(skip).limit(30);
+        res.status(200).send(product)
+    } catch (error) {
+        res.status(400).send({"msg": error.message})
+    }
+})
+
+
+// --------------->>>>>>>> Get Specific Product_type Product List <<<<<<<<-------------------
+
+productRouter.get("/gettype", auth, async (req, res) => {
+    try {
+        const {page, type} = req.query
+        let skip
+        if (page) 
+            skip = (page - 1) * 30;
+         else 
+            skip = 0
+
+        const product = await ProductModel.find({
+            product_type: {
+                $regex: `(?i)${type}`
+            }
+        }).skip(skip).limit(30);
+        res.status(200).send(product)
+    } catch (error) {
+        res.status(400).send({"msg": error.message})
+    }
+})
+
+
+// --------------->>>>>>>> Get Specific Tag_List Product List <<<<<<<<-------------------
+
+productRouter.get("/gettag", auth, async (req, res) => {
+    try {
+        const {page, tag} = req.query
+        let skip
+        if (page) 
+            skip = (page - 1) * 30;
+         else 
+            skip = 0
+
+        const product = await ProductModel.find({
+            tag_list: {
+                $regex: `(?i)${tag}`
+            }
+        }).skip(skip).limit(30);
+        res.status(200).send(product)
+    } catch (error) {
+        res.status(400).send({"msg": error.message})
+    }
+})
+
+
+// --------------->>>>>>>> Get Specific Brand Product List <<<<<<<<-------------------
+
+productRouter.get("/getbrand", auth, async (req, res) => {
+    try {
+        const {page, brand} = req.query
+        let skip
+        if (page) 
+            skip = (page - 1) * 30;
+         else 
+            skip = 0
+
+        const product = await ProductModel.find({
+            brand: {
+                $regex: `(?i)${brand}`
+            }
+        }).skip(skip).limit(30);
+        res.status(200).send(product)
+    } catch (error) {
+        res.status(400).send({"msg": error.message})
+    }
+})
+
+
+// --------------->>>>>>>> Get Specific Product  <<<<<<<<-------------------
+
+productRouter.get("/get/:_id", auth, async (req, res) => {
+    try {
+        const {_id} = req.params
+        const product = await ProductModel.find({_id})
+        res.status(200).send(product)
+    } catch (error) {
+        res.status(400).send({"msg": error.message})
+    }
+})
 
 // --------------->>>>>>>> Add Product <<<<<<<<-------------------
 
@@ -56,7 +149,6 @@ productRouter.patch("/update/:_id", admin, async (req, res) => {
         res.status(200).send({"msg": "Product Details Updated"})
     } catch (error) {
         res.status(400).send({"msg": error.message})
-
     }
 })
 
