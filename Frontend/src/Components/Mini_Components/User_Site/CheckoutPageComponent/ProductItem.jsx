435461bedc5c858f4b9cbac36098ef6f0beada15
uttamkrshaw <uttamkr5599@gmail.com>
2023-05-30 02:31:22 +0530

    order summary fixed

--- Frontend/src/Components/Mini_Components/User_Site/CheckoutPageComponent/ProductItem.jsx
+++ Frontend/src/Components/Mini_Components/User_Site/CheckoutPageComponent/ProductItem.jsx
@@ -3,32 +3,21 @@ import * as React from 'react'
 
 export const ProductItem = (props) => {
   const { product } = props
+  console.log("producert",product);
   return (
     <Flex justify="space-between" key={product.id}>
       <Stack direction="row" spacing="5">
         <AspectRatio ratio={1} width="92px">
-          <Image src={product.imageUrl} alt={product.name} borderRadius="md" />
+          <Image src={product.image_link} alt={product.name} borderRadius="md" />
         </AspectRatio>
         <Stack spacing="3">
           <Stack spacing="1">
             <Text fontWeight="semibold">{product.name}</Text>
-            <Text color={useColorModeValue('gray.600', 'gray.300')}>Size: 20</Text>
+            <Text color={useColorModeValue('gray.600', 'gray.300')}>Quantity: {product.Quantity}</Text>
           </Stack>
-          <Select
-            aria-label="Select quantity"
-            maxW="16"
-            size="sm"
-            borderRadius="md"
-            focusBorderColor={useColorModeValue('blue.500', 'blue.200')}
-            bg={useColorModeValue('white', 'gray.700')}
-          >
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-          </Select>
         </Stack>
       </Stack>
-      <Text fontWeight="medium">$199.00</Text>
+      <Text fontWeight="medium">${product.price}</Text>
     </Flex>
   )
 }
